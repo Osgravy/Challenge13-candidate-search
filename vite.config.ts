efd6087f0af010efe4d0f1cc7257f8b1154e4c14
Osgood <mosgood88@gmail.com>
2025-05-19 19:11:59 -0400

    added designated port

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
   plugins: [react()],
-});
+  preview: {
+    host: '0.0.0.0', // Add this
+    port: 4173,      // Explicit port
+    strictPort: true // Fail if port is unavailable
+  }
+})
